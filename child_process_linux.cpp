3a5cdbf1b54a33d0aa0f5fe44ada1d7f769f2c1b
James Fantin-Hardesty <24646452+jfantinhardesty@users.noreply.github.com>
2024-05-16 15:36:44 -0600

    Cleanup code

--- child_process_linux.cpp
+++ child_process_linux.cpp
@@ -3,8 +3,8 @@
 #include <iostream>
 #include <stdio.h>
 #include <sys/wait.h>
-#include <unistd.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 CloudfuseMngr::CloudfuseMngr(std::string mountDir, std::string configFile) {
     this->mountDir = mountDir;
@@ -125,33 +125,6 @@ bool CloudfuseMngr::isMounted() {
 
     
     return false;
-}
-
-int main() {
-    // Run test with non existent config file
-    std::string mountDir = "/home/jfan/Desktop/mount";
-    std::string configFile = "notExist";
-    CloudfuseMngr errCf = CloudfuseMngr(mountDir, configFile);
-
-    std::cout << "\n" << "Starting cloudfuse with incorrect config file" << '\n';
-    std::cout << "Is cloudfuse installed: " << errCf.isInstalled() << '\n';
-    std::cout << "Try dry run: " << errCf.dryRun().errCode << '\n';
-    std::cout << "Try mount: " << errCf.mount().errCode << '\n';
-    std::cout << "Is cloudfuse mounted: " << errCf.isMounted() << '\n';
-    std::cout << "Try unmount: " << errCf.unmount().errCode << '\n';
-
-    // Run test with existing config file
-    configFile = "config.yaml";
-    CloudfuseMngr workingCf = CloudfuseMngr(mountDir, configFile);
-
-    std::cout << "\n" << "Starting cloudfuse with correct config file" << '\n';
-    std::cout << "Is cloudfuse installed: " << workingCf.isInstalled() << '\n';
-    std::cout << "Try dry run: " << workingCf.dryRun().errCode << '\n';
-    std::cout << "Try mount: " << workingCf.mount().errCode << '\n';
-    std::cout << "Is cloudfuse mounted: " << workingCf.isMounted() << '\n';
-    std::cout << "Try unmount: " << workingCf.unmount().errCode << '\n';
-
-    return 0;
 }
 
 #endif
