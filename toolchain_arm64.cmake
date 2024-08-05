8f91a766d6fbb8e2b4a6e3a15ec730021d9d49fe
James Fantin-Hardesty <24646452+jfantinhardesty@users.noreply.github.com>
2024-08-05 16:32:47 -0600

    Enable cross compiling arm (#38)
    * Fix build script to cross compile on arm
    
    * Add scripts to cross compile
    
    * Cleanup cross compiling support and release process
    
    * Test cross compile release
    
    * Use VCPKG_ROOT
    
    * Don't pass no-test option
    
    * Install cross compiler
    
    * Fix specifying compiler for arm
    
    * Remove testing on branch
    
    * Try to fix clang arm64 build on Ubuntu 20.04
    
    * Use gcc for clang compiler on Ubuntu 20.04 due to errors

--- toolchain_arm64.cmake
+++ toolchain_arm64.cmake
@@ -1,8 +1,20 @@
 ## Copyright © 2024 Seagate Technology LLC and/or its Affiliates
 ## Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
 
-set(CMAKE_C_COMPILER "/usr/bin/aarch64-linux-gnu-gcc")
-set(CMAKE_CXX_COMPILER "/usr/bin/aarch64-linux-gnu-g++")
+# Clang supports cross compiling, but needs to specify the target to compile to
+if(CMAKE_C_COMPILER MATCHES "clang")
+    set(CMAKE_C_FLAGS "--target=aarch64-linux-gnu")
+    set(CMAKE_CXX_FLAGS "--target=aarch64-linux-gnu")
+endif()
+
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
+set(CMAKE_CROSSCOMPILING TRUE)
+
+set(VCPKG_TARGET_ARCHITECTURE arm64)
+set(VCPKG_CRT_LINKAGE static)
+set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 
 # This is required by FindThreads CMake module.
 set(THREADS_PTHREAD_ARG "2" CACHE STRING "" FORCE)
---
+++ toolchain_x64.cmake
@@ -0,0 +1,10 @@
+## Copyright © 2024 Seagate Technology LLC and/or its Affiliates
+## Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
+
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR AMD64)
+
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
