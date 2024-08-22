e149ea71e246bb7a46dd40dbd142aa254ef7cfd7
Michael Habinsky <michael.habinsky@seagate.com>
2024-08-22 11:08:40 -0600

    format

--- src/plugin/settings/engine.cpp
+++ src/plugin/settings/engine.cpp
@@ -100,7 +100,8 @@ std::string Engine::manifestString() const
     "capabilities": ")json" +
                          buildCapabilities() + R"json(",
     "deviceAgentSettingsModel":
-)json" + kEngineSettingsModel + R"json(
+)json" + kEngineSettingsModel +
+                         R"json(
 }
 )json";
 
