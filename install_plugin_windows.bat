5225437db8e9d2df6bfa3d3bfa03a3ac8b8c6829
James Fantin-Hardesty <24646452+jfantinhardesty@users.noreply.github.com>
2025-03-06 16:38:08 -0700

    Fix windows install script (#79)
    * Fix windows install script
    
    * Move restart VMS after checking for installer
    
    * Remove stopping twice

--- install_plugin_windows.bat
+++ install_plugin_windows.bat
@@ -1,39 +1,39 @@
 :: Copyright © 2024 Seagate Technology LLC and/or its Affiliates
 @echo off
 
-:: Check / get privileges 
+:: Check / get privileges
 net file 1>NUL 2>NUL
 if not '%errorlevel%' == '0' (
-    powershell Start-Process -FilePath "%0" -verb runas >NUL 2>&1
+    echo Installation failed: Need to run as administrator
+	pause
     exit /b
-) else (
-    :: If we just got privileges, drill back into the directory where the script is
-    cd /d %~dp0
 )
+
+cd /d "%~dp0"
 
 :: Find the installer file
 for %%I in (cloudfuse*.exe) do (
-    set "installer=%%I"
+    set "installer=%%~I"
     break
 )
 
 :: Check if the installer file exists and if so install it
-if exist "%installer%" (
-    echo Installing Cloudfuse
-    "%installer%" /VERYSILENT /SUPPRESSMSGBOXES /NORESTART
-) else (
-    echo Installer file not found
+if not exist "%installer%" (
+    echo Installation failed: Installer file not found
     pause
     exit /b
 )
-
-echo Installing plugin
 
 :: Stop the VMS server
 set serviceName="metavmsMediaServer"
 echo Attempting to stop %serviceName%
 net stop %serviceName% >NUL 2>&1
 echo Service stopped successfully.
+
+echo Installing Cloudfuse
+"%installer%" /VERYSILENT /SUPPRESSMSGBOXES /NORESTART
+
+echo Installing plugin
 
 :: Copy the plugin file
 echo Attempting to copy the plugin file
