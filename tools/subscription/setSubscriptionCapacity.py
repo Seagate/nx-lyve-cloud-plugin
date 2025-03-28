ccea1b75531141d0b9f1972368a992031de27393
Michael Habinsky <foodprocessor@users.noreply.github.com>
2025-03-28 15:16:44 -0600

    Rewrite subscription tools to handle multiple subscriptions at once (#22)

--- tools/subscription/setSubscriptionCapacity.py
+++ tools/subscription/setSubscriptionCapacity.py
@@ -10,19 +10,29 @@ def main():
     parser.add_argument('capacity_gb', nargs='?', type=int, default=2000, help='Desired storage subscription capacity (default 2000 GB)')
     args = parser.parse_args()
 
-    # parse subscription data from stdin
-    try:
-        subscription_info = json.loads(sys.stdin.read())
-    except Exception as e:
-        print(f'Error parsing subscription data: {type(e)}: {e}', file=sys.stderr)
-        return 1
-    
-    # add the capacity
-    subscription_info['bucket-capacity-gb'] = args.capacity_gb
-
-    # serialize as JSON and send to stdout
-    print(json.dumps(subscription_info))
-    return 0
+    # gather all input first (read until EOF)
+    subscriptionsString = sys.stdin.read()
+    # initialize
+    decoder = json.JSONDecoder()
+    startPosition = 0
+    objectsParsed = 0
+    while True:
+        try:
+            # skip whitespace
+            while startPosition + 1 < len(subscriptionsString) and subscriptionsString[startPosition].isspace():
+                startPosition += 1
+            # parse the next subscription JSON object
+            subscription_info, startPosition = decoder.raw_decode(subscriptionsString, startPosition)
+            objectsParsed += 1
+            # add the capacity
+            subscription_info['bucket-capacity-gb'] = args.capacity_gb
+            # serialize as JSON and send to stdout
+            print(json.dumps(subscription_info))
+        except Exception as e:
+            if objectsParsed == 0:
+                print(f'No valid JSON input data found. {type(e)}: {e}', file=sys.stderr)
+                return 1
+            return 0
 
 if __name__ == '__main__':
     # call main function
--- tools/subscription/signSubscription.py
+++ tools/subscription/signSubscription.py
@@ -6,11 +6,10 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 import argparse
 
-def sign_data(subscription_data, private_key_path):
+def sign_data(subscription_data, private_key_path, passphrase):
     # Prompt the user for the passphrase securely
     try:
         with open(private_key_path, 'rb') as keyFile:
-            passphrase = getpass.getpass(prompt='Enter passphrase (empty for no passphrase): ').encode()
             private_key = serialization.load_ssh_private_key(
                 keyFile.read(), password=passphrase, backend=default_backend()
             )
@@ -30,33 +29,53 @@ def main():
     parser.add_argument('private_key_path', help='path to private key file')
     args = parser.parse_args()
 
-    # parse subscription data from stdin
-    try:
-        subscription_data = json.dumps(json.loads(sys.stdin.read()), sort_keys=True).encode()
-    except Exception as e:
-        print(f'Error parsing subscription data: {type(e)}: {e}', file=sys.stderr)
-    
-    # get a signature
-    signature = sign_data(subscription_data, args.private_key_path)
-    if not signature:
-        print('Failed to sign subscription', file=sys.stderr)
-        return 1
-    
-    # combine subscription info and signature
-    subscription_data_base64 = base64.b64encode(subscription_data).decode()
-    combined_data = {
-        'subscriptionInfo': subscription_data_base64,
-        'signature': signature,
-    }
-    subscription_key = base64.b64encode(json.dumps(combined_data).encode()).decode()
-    
-    # print key (or report failure)
-    if subscription_key:
-        print(subscription_key)
-        return 0
-    else:
-        print('Failed to generate subscription key.', file=sys.stderr)
-        return 1
+    # gather all input first (read until EOF)
+    subscriptionsString = sys.stdin.read()
+    # initialize
+    passphrase = getpass.getpass(prompt='Enter private key passphrase (empty for no passphrase): ').encode()
+    decoder = json.JSONDecoder()
+    startPosition = 0
+    objectsParsed = 0
+    while True:
+        try:
+            # skip whitespace
+            while startPosition + 1 < len(subscriptionsString) and subscriptionsString[startPosition].isspace():
+                startPosition += 1
+            # parse the next subscription JSON object
+            subscription_info, startPosition = decoder.raw_decode(subscriptionsString, startPosition)
+            objectsParsed += 1
+            # serialize it in a standard way
+            subscription_data = json.dumps(subscription_info, sort_keys=True).encode()
+            
+            # get a signature
+            signature = sign_data(subscription_data, args.private_key_path, passphrase)
+            if not signature:
+                print('Failed to sign subscription', file=sys.stderr)
+                return 1
+            
+            # combine subscription info and signature
+            subscription_data_base64 = base64.b64encode(subscription_data).decode()
+            combined_data = {
+                'subscriptionInfo': subscription_data_base64,
+                'signature': signature,
+            }
+            subscription_key = base64.b64encode(json.dumps(combined_data).encode()).decode()
+            
+            # print key (or report failure)
+            if subscription_key:
+                print(subscription_key)
+            else:
+                print('Failed to generate subscription key.', file=sys.stderr)
+                return 1
+            
+        except json.decoder.JSONDecodeError as e:
+            if objectsParsed == 0:
+                print(f'No valid JSON input data found: {type(e)}: {e}', file=sys.stderr)
+                return 1
+            return 0
+        except Exception as e:
+            print(f"Failed to generate subscription key. Here\'s why: {type(e)}: {e}", file=sys.stderr)
+            return 1
 
 if __name__ == '__main__':
     # call main function
